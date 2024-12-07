e899ea9160b0a86844a0c51fd60769c2f281a07b
vanton <vanton.work@gmail.com>
2024-12-07 22:52:44 +0800

    create !webui-user.bat and project-words.txt

---
+++ !webui-user.bat
@@ -0,0 +1,22 @@
+@echo off
+
+set PYTHON=
+set GIT=
+set VENV_DIR=
+set COMMANDLINE_ARGS=
+
+@REM Uncomment following code to reference an existing A1111 checkout.
+@REM set A1111_HOME=Your A1111 checkout dir
+@REM
+@REM set VENV_DIR=%A1111_HOME%/venv
+@REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% ^
+@REM  --ckpt-dir %A1111_HOME%/models/Stable-diffusion ^
+@REM  --hypernetwork-dir %A1111_HOME%/models/hypernetworks ^
+@REM  --embeddings-dir %A1111_HOME%/embeddings ^
+@REM  --lora-dir %A1111_HOME%/models/Lora
+
+set "WEBUI_RICH_EXCEPTIONS=1"
+set "SD_WEBUI_RICH_LOG=1"
+set "SD_WEBUI_LOG_LEVEL=INFO"
+
+call webui.bat
